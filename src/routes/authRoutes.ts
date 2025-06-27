a5dde530a2c3a29612401e3c1308a119453a559c
Erick Carvalho <erick.carvalho@teamsoft.com.br>
2025-06-27 16:25:43 -0300

    Atualiza as rotas protegidas para permitir acesso de ADMIN ao perfil e remove rotas desnecessárias

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -2,7 +2,5 @@ import { UserRole } from '@/shared/constants/enums';
 
 export const protectedRoutes: Record<string, UserRole[]> = {
   '/dashboard': [UserRole.ADMIN],
-  '/profile': [UserRole.USER],
-  '/profile2': [UserRole.USER],
-  '/profile3': [UserRole.USER, UserRole.ADMIN],
+  '/profile': [UserRole.USER, UserRole.ADMIN],
 };
